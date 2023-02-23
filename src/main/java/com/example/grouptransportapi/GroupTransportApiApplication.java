6bf4f9e095ab1b4466de0804e9da5cb645dfa6d0
RoderCode <Rodersnow@gmail.com>
2023-02-23 10:40:42 +0100

    add config in application.properties

--- src/main/java/com/example/grouptransportapi/GroupTransportApiApplication.java
+++ src/main/java/com/example/grouptransportapi/GroupTransportApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GroupTransportApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GroupTransportApiApplication.class, args);
     }
-
 }
