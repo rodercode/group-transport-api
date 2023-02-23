7ba0e4dbeb4808ff892b565f7efa5cf83fd1b2e6
rodercode <rodersnow@gmail.com>
2023-02-23 10:27:25 +0100

    Initial commit

---
+++ src/main/java/com/example/grouptransportapi/GroupTransportApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.grouptransportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupTransportApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupTransportApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grouptransportapi/GroupTransportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grouptransportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupTransportApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
