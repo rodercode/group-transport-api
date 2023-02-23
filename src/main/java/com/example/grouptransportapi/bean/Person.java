cd1920ed4adfa6a1353d818d87b48f3cee4082e7
RoderCode <Rodersnow@gmail.com>
2023-02-23 20:45:12 +0100

    add Person Entity

---
+++ src/main/java/com/example/grouptransportapi/bean/Person.java
@@ -0,0 +1,25 @@
+package com.example.grouptransportapi.bean;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity(name = "persons")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstname;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+}
