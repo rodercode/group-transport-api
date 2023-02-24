50cf6b0be26ce1a00797eefb681bc77e6305cfdf
RoderCode <Rodersnow@gmail.com>
2023-02-24 11:31:13 +0100

    add exceptionHandler for Resource not found exception

--- src/main/java/com/example/grouptransportapi/exception/GroupRestExceptionHandler.java
+++ src/main/java/com/example/grouptransportapi/exception/GroupRestExceptionHandler.java
@@ -3,6 +3,8 @@ package com.example.grouptransportapi.exception;
 
 import com.example.grouptransportapi.bean.GroupErrorResponse;
 import com.example.grouptransportapi.handler.ListEmptyException;
+import com.example.grouptransportapi.handler.ResourceNotFoundException;
+import jakarta.annotation.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,10 +16,21 @@ import java.time.ZonedDateTime;
 @ControllerAdvice
 public class GroupRestExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity<GroupErrorResponse> handleException(ListEmptyException e){
+    public ResponseEntity<GroupErrorResponse> handleException(ListEmptyException e) {
         GroupErrorResponse error = new GroupErrorResponse
                 (
                         HttpStatus.NO_CONTENT.value(),
+                        e.getMessage(),
+                        ZonedDateTime.now(ZoneId.of("Z"))
+                );
+        return new ResponseEntity<>(error, HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<GroupErrorResponse> handleException(ResourceNotFoundException e) {
+        GroupErrorResponse error = new GroupErrorResponse
+                (
+                        HttpStatus.NOT_FOUND.value(),
                         e.getMessage(),
                         ZonedDateTime.now(ZoneId.of("Z"))
                 );
