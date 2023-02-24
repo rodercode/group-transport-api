72a2cbce6fd7d68c72f9dc118ffe4a1a2dda1649
RoderCode <Rodersnow@gmail.com>
2023-02-24 22:18:58 +0100

    update create vehicle crud method

--- src/main/java/com/example/grouptransportapi/service/VehicleService.java
+++ src/main/java/com/example/grouptransportapi/service/VehicleService.java
@@ -1,6 +1,8 @@
 package com.example.grouptransportapi.service;
 
+import com.example.grouptransportapi.bean.Crew;
 import com.example.grouptransportapi.bean.Vehicle;
+import com.example.grouptransportapi.dao.CrewRepository;
 import com.example.grouptransportapi.dao.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,12 +13,20 @@ import java.util.Optional;
 @Service
 public class VehicleService {
     private final VehicleRepository vehicleRepo;
+    private final CrewService crewService;
+    private final CrewRepository crewRepository;
     @Autowired
-    public VehicleService(VehicleRepository vehicleRepo) {
+    public VehicleService(VehicleRepository vehicleRepo, CrewService crewService,
+                          CrewRepository crewRepository) {
         this.vehicleRepo = vehicleRepo;
+        this.crewService = crewService;
+        this.crewRepository = crewRepository;
     }
-
     public void createVehicle(Vehicle vehicle){
+        Crew crew = crewService.showGroupById(vehicle.getGroupId()).get();
+        int vehicles = crew.getVehicle();
+        crew.setVehicle(vehicles + 1);
+        crewRepository.save(crew);
         vehicleRepo.save(vehicle);
     }
     public List<Vehicle> selectVehicles(){
