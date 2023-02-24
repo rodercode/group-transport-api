a4abd2a481ec4002af40955554fde34043e31be5
RoderCode <Rodersnow@gmail.com>
2023-02-24 19:17:49 +0100

    create vehicle service and a create method in service

--- src/main/java/com/example/grouptransportapi/dao/VehicleRepository.java
+++ src/main/java/com/example/grouptransportapi/dao/VehicleRepository.java
@@ -2,6 +2,8 @@ package com.example.grouptransportapi.dao;
 
 import com.example.grouptransportapi.bean.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 }
---
+++ src/main/java/com/example/grouptransportapi/service/VehicleService.java
@@ -0,0 +1,19 @@
+package com.example.grouptransportapi.service;
+
+import com.example.grouptransportapi.bean.Vehicle;
+import com.example.grouptransportapi.dao.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleService {
+    private final VehicleRepository vehicleRepo;
+    @Autowired
+    public VehicleService(VehicleRepository vehicleRepo) {
+        this.vehicleRepo = vehicleRepo;
+    }
+
+    public void createVehicle(Vehicle vehicle){
+        vehicleRepo.save(vehicle);
+    }
+}
