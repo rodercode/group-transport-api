5ad25b4edf7f3e121b7324ccff7508bf3ac3d113
RoderCode <Rodersnow@gmail.com>
2023-02-24 08:42:57 +0100

    get request method show all current groups

--- src/main/java/com/example/grouptransportapi/api/GroupRestController.java
+++ src/main/java/com/example/grouptransportapi/api/GroupRestController.java
@@ -3,10 +3,9 @@ package com.example.grouptransportapi.api;
 import com.example.grouptransportapi.bean.Group;
 import com.example.grouptransportapi.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class GroupRestController {
@@ -15,6 +14,10 @@ public class GroupRestController {
     @Autowired
     public GroupRestController(GroupService groupService) {
         this.groupService = groupService;
+    }
+    @GetMapping("api/groups")
+    private List<Group> showAllGroups(){
+       return groupService.showGroups();
     }
     @PostMapping("api/groups")
     private String createGroup(Group group){
