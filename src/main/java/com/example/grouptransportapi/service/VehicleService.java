a6642bec6846a335f9057a8dbe2df37d7ea428d6
RoderCode <Rodersnow@gmail.com>
2023-02-25 10:36:04 +0100

    change location on read crud method

--- src/main/java/com/example/grouptransportapi/service/VehicleService.java
+++ src/main/java/com/example/grouptransportapi/service/VehicleService.java
@@ -13,28 +13,28 @@ import java.util.Optional;
 @Service
 public class VehicleService {
     private final VehicleRepository vehicleRepo;
-    private final CrewService crewService;
     private final CrewRepository crewRepository;
     @Autowired
-    public VehicleService(VehicleRepository vehicleRepo, CrewService crewService,
-                          CrewRepository crewRepository) {
+    public VehicleService(VehicleRepository vehicleRepo, CrewRepository crewRepository) {
         this.vehicleRepo = vehicleRepo;
-        this.crewService = crewService;
         this.crewRepository = crewRepository;
     }
-    public void createVehicle(Vehicle vehicle){
-        Crew crew = crewService.showGroupById(vehicle.getGroupId()).get();
-        int vehicles = crew.getVehicle();
-        crew.setVehicle(vehicles + 1);
-        crewRepository.save(crew);
-        vehicleRepo.save(vehicle);
-    }
+
     public List<Vehicle> selectVehicles(){
         return vehicleRepo.findAll();
     }
     public List<Vehicle> selectVehiclesByGroupId(Long groupId){
         return vehicleRepo.findAllByGroupId(groupId);
     }
+
+    public void createVehicle(Vehicle vehicle){
+        Crew crew = crewRepository.findById(vehicle.getGroupId()).get();
+        int vehicles = crew.getVehicle();
+        crew.setVehicle(vehicles + 1);
+        crewRepository.save(crew);
+        vehicleRepo.save(vehicle);
+    }
+
 
     public void removeVehicle(Long groupId){
        Vehicle vehicle = vehicleRepo.findById(groupId).get();
