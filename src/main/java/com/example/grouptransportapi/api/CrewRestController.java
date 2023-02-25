4c464a5bc10c12c9d27b10a86583f6dad8f687c4
RoderCode <Rodersnow@gmail.com>
2023-02-25 11:53:39 +0100

    fix bug delete group Rest API

--- src/main/java/com/example/grouptransportapi/api/CrewRestController.java
+++ src/main/java/com/example/grouptransportapi/api/CrewRestController.java
@@ -1,44 +1,34 @@
 package com.example.grouptransportapi.api;
-
 import com.example.grouptransportapi.bean.Crew;
 import com.example.grouptransportapi.bean.Vehicle;
-import com.example.grouptransportapi.dao.CrewRepository;
 import com.example.grouptransportapi.service.CrewService;
 import com.example.grouptransportapi.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController
 @RequestMapping("api/groups")
 public class CrewRestController {
     private final CrewService crewService;
     private final VehicleService vehicleService;
-    private final CrewRepository crewRepository;
-
     @Autowired
-    public CrewRestController(CrewService groupService, VehicleService vehicleService,
-                              CrewRepository crewRepository) {
-        this.crewService = groupService;
+    public CrewRestController(CrewService crewService, VehicleService vehicleService) {
+        this.crewService = crewService;
         this.vehicleService = vehicleService;
-        this.crewRepository = crewRepository;
     }
-
     @GetMapping
     private ResponseEntity<List<Crew>> showAllGroups() {
         return ResponseEntity.ok(crewService.showGroups());
     }
-
     @PostMapping
     private ResponseEntity<Crew> createGroup(@RequestBody Crew group) {
         crewService.createGroup(group);
         return new ResponseEntity<>(group, HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/{groupId}")
+    @DeleteMapping("{groupId}")
     private ResponseEntity<Crew> deleteGroup(@PathVariable Long groupId) {
         Crew crew = crewService.showGroupById(groupId).get();
         crewService.removeGroup(crew);
