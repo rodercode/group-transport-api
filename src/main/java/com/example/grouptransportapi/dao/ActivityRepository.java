48ba786d2653656cbdb5fd7826680701a56ace80
RoderCode <Rodersnow@gmail.com>
2023-02-26 13:17:58 +0100

    add @Repo annotions

--- src/main/java/com/example/grouptransportapi/dao/ActivityRepository.java
+++ src/main/java/com/example/grouptransportapi/dao/ActivityRepository.java
@@ -1,7 +1,8 @@
 package com.example.grouptransportapi.dao;
-
 import com.example.grouptransportapi.bean.Walk;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityRepository extends JpaRepository<Walk,Long> {
 }
